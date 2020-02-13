629b921f0b2fe2cd2422f8b9424e8c695e45c11f
Greg Shanks <greg.shanks@gmail.com>
2020-02-13 11:20:44 +0000

    Initial Commit

---
+++ src/test/java/com/example/codeclan/coursebookingservice/CoursebookingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.coursebookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursebookingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
